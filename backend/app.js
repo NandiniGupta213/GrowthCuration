dd9ba9d86b85d37df4504c166543779e906ea861
Nandini Gupta <ng3557879@gmail.com>
2025-05-10 23:56:46 +0530

    chamges in title

--- backend/app.js
+++ backend/app.js
@@ -22,20 +22,40 @@ app.use(session({
       maxAge: 3600000 
     }
   }));
-  
-app.use(cors({
-    origin: 'http://127.0.0.1:5500', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],       
-    credentials: true  
+
+
+  app.use(cors({
+    origin: ['https://growthcuration-frontend5.onrender.com', 'http://127.0.0.1:5500'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
   }));
+  
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static('public'));
 
 
 
 app.use("/contactform",ContactForm)
 app.use("/Review",Reviews)
 
+
+
+app.get('/', (req, res) => {
+  res.send('Backend is up and running');
+});
+
+
+// Catch 404 errors
+app.use((req, res, next) => {
+  res.status(404).send({ error: 'Not Found' });
+});
+
+// General error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: 'Something went wrong!' });
+});
 
 
 
